ace85dc752b889f07d45b9e86d4b2705ec4c0a6b
Rocket13011 <alexandre@jeedom.com>
2023-03-16 10:48:29 +0100

    avec update

--- data/patchs/scripts/resetFactory.sh
+++ data/patchs/scripts/resetFactory.sh
@@ -1,2 +1,4 @@
+sudo wget https://github.com/jeedom/plugin-luna/blob/beta/data/patchs/root/usr/bin/fsreset -O /userdata/fsreset
+sudo cp /userdata/fsreset /usr/bin/fsreset
 sudo fsreset
 sudo reboot
