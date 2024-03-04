09a5c19708446617e701559cf3b622d6f2625108
Rocket13011 <alexandre@jeedom.com>
2024-03-04 16:31:07 +0100

    Correction double IP

--- data/patchs/patchLuna.sh
+++ data/patchs/patchLuna.sh
@@ -38,4 +38,8 @@ sudo systemctl restart done
 php /var/www/html/core/php/jeecli.php message add "luna" "correction dnsmasq sur la luna"
 sudo apt-get remove -y dnsmasq
 
+#wmsgd.service
+php /var/www/html/core/php/jeecli.php message add "luna" "correction double ip sur la luna"
+sudo systemctl disable wmsgd.service
+
 return 0
